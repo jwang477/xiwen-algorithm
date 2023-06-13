b2eadff776602d11422995f476e237254d5e7448
WangXiWen1029 <treasure1029@sina.cn>
2023-06-13 11:32:43 +0800

    update

--- src/test/java/com/wang/leetcode/LeetCode_03.java
+++ src/test/java/com/wang/leetcode/LeetCode_03.java
@@ -1,2 +1,58 @@
-package com.wang.leetcode;public class LeetCode_03 {
+package com.wang.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode_03 {
+
+    public static void main(String[] args) {
+        String str = "aab";
+        int count = lengthOfLongestSubstring(str);
+        System.out.println("count = " + count);
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int count = 0;
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0, temp = 0; i < s.length(); i++) {
+            if (set.add(s.charAt(i))) {
+                count++;
+            } else {
+                max = Integer.max(count, max);
+                count = 0;
+                i = temp;
+                temp++;
+                set.clear();
+            }
+        }
+        return Integer.max(count, max);
+    }
+
 }
--- src/test/java/com/wang/leetcode/LeetCode_2475.java
+++ src/test/java/com/wang/leetcode/LeetCode_2475.java
@@ -1,2 +1,49 @@
-package com.wang.leetcode;public class LeetCode_2475 {
+package com.wang.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个下标从 0 开始的正整数数组 nums 。请你找出并统计满足下述条件的三元组 (i, j, k) 的数目：
+ * <p>
+ * 0 <= i < j < k < nums.length
+ * nums[i]、nums[j] 和 nums[k] 两两不同 。
+ * 换句话说：nums[i] != nums[j]、nums[i] != nums[k] 且 nums[j] != nums[k] 。
+ * 返回满足上述条件三元组的数目。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/number-of-unequal-triplets-in-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 3 <= nums.length <= 100
+ * 1 <= nums[i] <= 1000
+ */
+public class LeetCode_2475 {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 4, 2, 4, 3};
+        int count = unequalTriplets(nums);
+        System.out.println("count = " + count);
+    }
+
+    public static int unequalTriplets(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[k] != nums[j]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int unequalTripletsV2(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+
+
+        return count;
+    }
+
 }
